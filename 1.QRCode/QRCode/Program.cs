ff3a8c4a98ef201da4a53234e3781f7b3376324a
yxc <xinchao.yuan@techcreate.net>
2019-09-02 11:12:14 +0800

    二维码生成小工具

---
+++ 1.QRCode/QRCode/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Drawing;
+using System.Text;
+using ThoughtWorks.QRCode.Codec;
+using ThoughtWorks.QRCode.Codec.Data;//引用的第三方dll
+
+namespace QRCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string content = "嘿嘿嘿!\n科为创通科技股份有限公司(techcreate)\n项目总监:赵鲲翔";
+            QRCodeEncoderUtil(content);
+        }
+
+        /// <summary> 
+        /// 生成二维码 
+        /// </summary> 
+        /// <param name="qrCodeContent">要编码的内容</param> 
+        /// <returns>返回二维码位图</returns> 
+        public static Bitmap QRCodeEncoderUtil(string qrCodeContent)
+        {
+            QRCodeEncoder qrCodeEncoder = new QRCodeEncoder();
+            qrCodeEncoder.QRCodeScale = 5;
+            qrCodeEncoder.QRCodeVersion = 0;
+            Bitmap img = qrCodeEncoder.Encode(qrCodeContent, Encoding.UTF8);//指定utf-8编码， 支持中文 
+            img.Save(@"E:\temp.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            return img;
+        }
+
+        /// <summary> 
+        /// 解析二维码 
+        /// </summary> 
+        /// <param name="bitmap">要解析的二维码位图</param> 
+        /// <returns>解析后的字符串</returns> 
+        public static string QRCodeDecoderUtil(Bitmap bitmap)
+        {
+            QRCodeDecoder decoder = new QRCodeDecoder();
+            string decodedString = decoder.decode(new QRCodeBitmapImage(bitmap), Encoding.UTF8);//指定utf-8编码， 支持中文
+            return decodedString;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # .NET-Tools
-.net平台下的小工具类
+.net平台下的小工具类,使用C#语言开发的前/后端小工具类.
+##1.生成二维码
+ThoughtWorks.QRCode.dll是第三方组件.
+
+##2.
